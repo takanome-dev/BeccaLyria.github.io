5ef6123cc93cfebaca2d466ad40a7b315b287b8a
Nicholas Carrigan <ncharrigan@gmail.com>
2021-02-20 15:36:10 -0800

    Auto-generated commit

---
+++ assets/data/artList.ts
@@ -0,0 +1,45 @@
+export interface ArtInt {
+  fileName: string;
+  artName: string;
+  artist: string;
+  artistUrl: string;
+}
+
+export const artList: ArtInt[] = [
+  {
+    fileName: 'Moonlight 1.png',
+    artName: 'Becca Lyria',
+    artist: 'Moonlight',
+    artistUrl: 'https://www.instagram.com/moonlightkcreations/',
+  },
+  {
+    fileName: 'Moonlight 3.png',
+    artName: 'A Forest Adventure',
+    artist: 'Moonlight',
+    artistUrl: 'https://www.instagram.com/moonlightkcreations/',
+  },
+  {
+    fileName: 'Moonlight 4.png',
+    artName: 'A Day at the Beach',
+    artist: 'Moonlight',
+    artistUrl: 'https://www.instagram.com/moonlightkcreations/',
+  },
+  {
+    fileName: 'Moonlight 5.png',
+    artName: 'Princess Becca',
+    artist: 'Moonlight',
+    artistUrl: 'https://www.instagram.com/moonlightkcreations/',
+  },
+  {
+    fileName: 'Moonlight 6.jpg',
+    artName: 'Sketch',
+    artist: 'Moonlight',
+    artistUrl: 'https://www.instagram.com/moonlightkcreations/',
+  },
+  {
+    fileName: 'Starfazers.png',
+    artName: 'Untitled',
+    artist: 'Starfazers',
+    artistUrl: 'https://instagram.com/starfazers',
+  },
+];
---
+++ assets/data/bio.ts
@@ -0,0 +1,31 @@
+/* eslint-disable max-len */
+export interface BioInt {
+  category: string;
+  data: string;
+}
+
+export const bioData: BioInt[] = [
+  {
+    category: 'Introduction',
+    data: `I am a young female wizard. Fuelled primarily by the pursuit of knowledge, I travel the multiverse always searching for new things to learn. I'm known for being friendly to those I meet, but always keeping them at a distance. Aside from Rosalia, I have no close, trusted companions. I have been seen travelling with others, but only when the situation calls for it. My true age is unknown (even I cannot remember), but my physical age remains 21.`,
+  },
+  {
+    category: 'History',
+    data:
+      'I was an orphan, growing up in the streets of a small village. As a child, I had to struggle to survive day to day. It is partly because of this that I struggle to form deep emotional bonds with others - I find it hard to trust folks I do not know well. When I was 8, I had been caught by the town guard attempting to steal food. In the resulting altercation, I unintentionally called lightning down on a guardsman. This unexpected spurt of magical ability caught the attention of the nearest Academy, and I was sent there for magical training. I spent 10 years there studying - where I strengthened my control over lightning. During my time in the Academy, I also began exploring necromancy. My professors caught wind of this and prohibited me from continuing the exploration of that field. Being denied the opportunity to pursue more knowledge, I became bitter and frustrated about the restrictions imposed upon me. When I came across the methods for interplanar travel, I bid my farewells to the Academy and set off to continue my studies.',
+  },
+  {
+    category: 'Appearance',
+    data:
+      'I have soft white hair which I keep in pigtails, deep purple eyes, and pale skin. I wear glasses, despite not needing them, and can always be seen in purple clothing - usually a dress, but sometimes other outfits. The outfits I wear tend to be cute and elegant; I enjoy the attention that dressing up results in. When I am not travelling, I will wear more comfortable clothing. I wear purple lipstick and eye shadow, and keep my fingernails painted purple as well. I sometimes smile, but more often maintains a neutral expression with a focused gaze - I am always observing my surroundings and keeping a close watch on the things going on.',
+  },
+  {
+    category: 'Skills and Combat',
+    data: `I am a fulgramancer and necromancer - I specialise in lightning and death magic, and am capable of bringing destruction to my foes from a distance. I prefer to avoid close combat, and will employ stealth as needed to avoid being seen by my targets. My magical prowess has granted me near-immortality. It is believed that I cannot be killed, though when I suffer what would be a fatal blow I must retreat to a private plane of existence. There I take the time needed to absorb the magical energies of the planes and recover my strength. I will sometimes use a wand or staff to help channel my magical energies, but can work just as well when I am completely unarmed.`,
+  },
+  {
+    category: 'Personality',
+    data:
+      'I fall under the Lawful Neutral alignment. I will not go out of my way to help others, but will also not unnecessarily inflict harm (things such as theft and murder are not my methods). I WILL, however, do whatever is necessary to fulfil a job contract or request. Once someone has requested my services and promised payment, I consider that to be the establishment of a contract and will complete the task without fail. I cannot be persuaded to abandon the contract or betray the employer, regardless of any moral or ethical reasons to do so. This fierce loyalty to my clients has earned me a strong reputation as reliable, and I frequently see requests for work. I will often turn down requests, though, should they require me to step away from my quest for learning.',
+  },
+];
