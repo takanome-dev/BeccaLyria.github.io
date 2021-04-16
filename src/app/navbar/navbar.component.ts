8a10a3ea991abe3d986fc9313f6b24a2b29a9f70
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-04-15 18:21:45 -0700

    feat: add version
    Display the page version in the navbar!

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { version } from '../../../package.json';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  public version = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.version = version;
   }
-
 }
