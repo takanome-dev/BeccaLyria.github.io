c882b662d2736842546bb10155130f4a63f0bf64
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-05-08 11:21:15 -0700

    feat: additional games
    Add a couple of extra games.

--- src/assets/data/gameList.ts
+++ src/assets/data/gameList.ts
@@ -16,6 +16,11 @@ export const GameList: GameInt[] = [
     gameUrl: 'https://store.steampowered.com/app/678960/CODE_VEIN/',
   },
   {
+    fileName: 'conan-exiles.jpg',
+    gameName: 'Conan Exiles',
+    gameUrl: 'https://store.steampowered.com/app/440900/Conan_Exiles/',
+  },
+  {
     fileName: 'cyberpunk.png',
     gameName: 'CyberPunk 2077',
     gameUrl: 'https://store.steampowered.com/app/1091500/Cyberpunk_2077/',
@@ -36,6 +41,11 @@ export const GameList: GameInt[] = [
     fileName: 'guild-wars-2.png',
     gameName: 'Guild Wars 2',
     gameUrl: 'https://www.guildwars2.com/',
+  },
+  {
+    fileName: 'fenix-rising.png',
+    gameName: 'Immortals: Fenix Rising',
+    gameUrl: 'https://www.ubisoft.com/en-us/game/immortals-fenyx-rising',
   },
   {
     fileName: 'tyranny.png',
