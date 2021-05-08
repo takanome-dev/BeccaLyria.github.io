edfdf1a3ebc50419b5a2af9347ee0eb080ee2f94
Nicholas Carrigan <ncharrigan@gmail.com>
2021-05-08 18:48:25 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/BeccaLyria/BeccaLyria.github.io/commit/6932af9696fbea8f412d76e1a42fe8b54ab25a6c

---
+++ assets/data/gameList.ts
@@ -0,0 +1,55 @@
+export interface GameInt {
+  fileName: string;
+  gameName: string;
+  gameUrl: string;
+}
+
+export const GameList: GameInt[] = [
+  {
+    fileName: 'cladun.png',
+    gameName: 'Cladun X2',
+    gameUrl: 'https://store.steampowered.com/app/206250/Cladun_X2/',
+  },
+  {
+    fileName: 'code-vein.png',
+    gameName: 'CODE VEIN',
+    gameUrl: 'https://store.steampowered.com/app/678960/CODE_VEIN/',
+  },
+  {
+    fileName: 'conan-exiles.jpg',
+    gameName: 'Conan Exiles',
+    gameUrl: 'https://store.steampowered.com/app/440900/Conan_Exiles/',
+  },
+  {
+    fileName: 'cyberpunk.png',
+    gameName: 'CyberPunk 2077',
+    gameUrl: 'https://store.steampowered.com/app/1091500/Cyberpunk_2077/',
+  },
+  {
+    fileName: 'fire-emblem-fates.png',
+    gameName: 'Fire Emblem: Fates',
+    gameUrl:
+      'https://www.nintendo.com/games/detail/fire-emblem-fates-revelation-3ds/',
+  },
+  {
+    fileName: 'godeater.png',
+    gameName: 'GOD EATER',
+    gameUrl:
+      'https://store.steampowered.com/app/438490/GOD_EATER_2_Rage_Burst/',
+  },
+  {
+    fileName: 'guild-wars-2.png',
+    gameName: 'Guild Wars 2',
+    gameUrl: 'https://www.guildwars2.com/',
+  },
+  {
+    fileName: 'fenix-rising.png',
+    gameName: 'Immortals: Fenix Rising',
+    gameUrl: 'https://www.ubisoft.com/en-us/game/immortals-fenyx-rising',
+  },
+  {
+    fileName: 'tyranny.png',
+    gameName: 'Tyranny',
+    gameUrl: 'https://store.steampowered.com/app/362960/Tyranny/',
+  },
+];
